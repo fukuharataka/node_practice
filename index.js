6efc3d338eaab8e8fdfd37e8e5d224748d2c10d8
fukuharataka <chapio0817@gmail.com>
2019-02-11 14:07:05 +0900

    [Finish]4-2

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var ejs = require("ejs");
+
+var app = express();
+app.engine('ejs', ejs.renderFile);
+app.use(express.static('public'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+var data = {
+  'Taro'    : 'taro@yamada',
+  'Hanako'  : 'hanako@flower',
+  'Sachiko' : 'sachiko@happy',
+  'Ichiro'  : 'ichi@baseball',
+};
 
 app.get('/', (req, res)=>{
-  res.send('Welcome')
+  var msg = "This is Express Page!<br>"
+          + "※データを表示します";
+  res.render('index.ejs',
+    {title: 'Index',
+    content: msg,
+    data:data,
+  });
+});
+
+app.post('/',(req,res) => {
+  var msg = 'This is Posted Page<br>'+
+            'あなたは「'+ req.body.message +'」と送信しました'
+  res.render('index.ejs',
+    {title: 'Posted',
+    content: msg,
+  })
 })
 
-app.listen(3000, ()=>{
-  console.log('start')
+app.get('/other',(req,res)=>{
+  var name = req.query.name;
+  var pass = req.query.pass;
+  var msg = "あなたの名前は「"+ name +"」です<br>"
+    + "パスワードは「"+ pass +"」です";
+  res.render('index.ejs',
+  {title: 'other',
+  content: msg,
+  link:{href:'/', text:'※トップに戻る'}
+  });
+});
+
+var server = app.listen(3000, () => {
+  console.log('Server is running!');
 })
